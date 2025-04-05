09f5949afb22515bb84026baa269193e1115fb2e
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-04-05 17:13:06 +1300

    stick programs at right level

--- home/common.nix
+++ home/common.nix
@@ -3,12 +3,6 @@
 {
   home = {
 
-    programs = {
-      firefox.enable = true;
-      home-manager.enable = true; # Let home Manager install and manage itself.
-      bash.enable = true;
-    };
-
     # This value determines the home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new home Manager release introduces backwards
@@ -18,5 +12,11 @@
     # the home Manager release notes for a list of state version
     # changes in each release.
     stateVersion = "24.11";
+  };
+
+  programs = {
+    firefox.enable = true;
+    home-manager.enable = true; # Let home Manager install and manage itself.
+    bash.enable = true;
   };
 }
