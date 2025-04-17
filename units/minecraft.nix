f6fe18dd5b71ba30841ba3b483e209a2fc8d4777
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-04-17 22:26:30 +1200

    update pack

--- units/minecraft.nix
+++ units/minecraft.nix
@@ -2,7 +2,7 @@
 let
   modpack = pkgs.fetchPackwizModpack {
     url = "https://github.com/ArcOnyx/thm-modpack/raw/main/pack.toml";
-    packHash = "sha256-cuU/2uQ7s74JN3f3ilU7EFAsvYuJkQ88XCr0jEwNBwc=";
+    packHash = "sha256-+TEDndc5zvTAZ5k9WdRQnnc0qT9zpic86wSbk3N7TF0=";
   };
 in
 {
