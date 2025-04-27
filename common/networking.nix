411bee6b57ab9a1a384d07b0ed73d55a21d5b69d
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-04-27 15:12:04 +1200

    declarative networking

--- common/networking.nix
+++ common/networking.nix
@@ -2,7 +2,34 @@
 
 {
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    ensureProfiles.profiles = {
+      "wired" = {
+        connection = {
+          autoconnect = true;
+          autoconnect-priority = "500";
+          id = "wired";
+          interface-name = "eno1";
+          type = "ethernet";
+          uuid = "f98205ef-5e61-3c6c-b1df-eace732ce8ee";
+        };
+        ethernet = { };
+        ipv4 = {
+          method = "auto";
+          # route-metric = "20";
+          ignore-auto-dns = true;
+        };
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+          # route-metric = "20";
+          ignore-auto-dns = true;
+        };
+        proxy = { };
+      };
+    };
+  };
   networking.nameservers = [
     "8.8.8.8"
     "1.1.1.1"
