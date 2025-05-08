50b2932b0691b661c1d699f2792c1da4a7560178
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-05-08 16:56:20 +1200

    do i just need to be more generic?

--- units/minecraft/network.nix
+++ units/minecraft/network.nix
@@ -17,11 +17,45 @@
     apiTokenFile = "/etc/cloudflare/apikey.env";
   };
 
+  services.caddy.virtualHosts.":9010" = {
+    extraConfig = ''
+      encode
+
+      redir /exposure /exposure/
+      redir /map /map/
+
+      # let people browse their image exports
+      handle_path /exposure/* {
+        root * /srv/minecraft/magic/world/exposures
+        file_server browse
+      }
+
+      # bluemap, stripping prefix
+      handle_path /map/* {
+        reverse_proxy :8100
+      }
+
+      handle {
+        error 404
+      }
+
+      handle_errors {
+        respond "{err.status_code} {err.status_text}"
+      }
+    '';
+  };
+
   systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "minecraft" ];
+
+  services.tsnsrv.services.minecraft = {
+    funnel = true;
+    suppressWhois = true;
+    toURL = "http://127.0.0.1:9010";
+  };
 
   services.tsnsrv.services.map = {
     funnel = true;
-    suppressWhois = true; # we won't be using the info anyway
+    suppressWhois = true;
     toURL = "http://127.0.0.1:8100";
   };
 }
