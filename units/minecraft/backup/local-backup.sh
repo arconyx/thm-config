cd7b1fa62f3212b003d118c5c19a6015d7d952af
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-05-09 16:25:57 +1200

    try fix local backup script

--- units/minecraft/backup/local-backup.sh
+++ units/minecraft/backup/local-backup.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-bash ./before-backup.sh
+./before-backup.sh
 BACKUP_PATH="/srv/minecraft/backup/$SERVER_NAME/$(date --iso-8601=minutes)"
 mkdir -p "$BACKUP_PATH"
 cp -r "$DATA_PATH" "$BACKUP_PATH"
 # for the eventual cleanup script
 echo $(fd --type directory --exact-depth 1 --changed-before 1d)
-bash ./after-backup.sh
+./after-backup.sh
