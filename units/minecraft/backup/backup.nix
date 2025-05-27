b0c31c5c93fd872215287a5067064c2eba8dd2f9
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-05-27 18:39:16 +1200

    refactor local backup to use systemd dependencies

--- units/minecraft/backup/backup.nix
+++ units/minecraft/backup/backup.nix
@@ -16,103 +16,95 @@
   };
 
   # TODO: Parameterise server name
-  systemd.services =
-    let
-      before-backup = ./before-backup.sh;
-      after-backup = ./after-backup.sh;
-      local-backup = ''
-        ${before-backup}
+  systemd.services = {
+    minecraft-before-backup = {
+      enable = true;
+      description = "Prepare for restic backup of Minecraft server";
+      requisite = [
+        "minecraft-server-magic.service"
+        "minecraft-server-magic.socket"
+      ];
+      wantedBy = [ "restic-backups-backblaze.service" ];
+      before = [ "restic-backups-backblaze.service" ];
+      after = [
+        "minecraft-server-magic.service"
+        "minecraft-server-magic.socket"
+      ];
+      serviceConfig = {
+        User = config.services.minecraft-servers.user;
+        Group = config.services.minecraft-servers.group;
+        Type = "oneshot";
+      };
+      script = builtins.readFile ./before-backup.sh;
+      environment = {
+        ANNOUNCE = "1";
+        BACKUP_DEST = "Remote";
+        DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
+        SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
+        SAVE_WAIT_TIME = "60";
+        SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
+      };
+    };
+
+    minecraft-after-backup = {
+      enable = true;
+      description = "Cleanup after restic backup of Minecraft server";
+      requisite = [
+        "minecraft-server-magic.service"
+        "minecraft-server-magic.socket"
+      ];
+      wantedBy = [ "restic-backups-backblaze.service" ];
+      after = [ "restic-backups-backblaze.service" ];
+      serviceConfig = {
+        User = config.services.minecraft-servers.user;
+        Group = config.services.minecraft-servers.group;
+        Type = "oneshot";
+      };
+      script = builtins.readFile ./after-backup.sh;
+      environment = {
+        ANNOUNCE = "1";
+        BACKUP_DEST = "Remote";
+        DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
+        SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
+        SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
+      };
+    };
+
+    minecraft-local-backup = {
+      enable = true;
+      description = "Backup Minecraft server to a local folder";
+      requisite = [
+        "minecraft-server-magic.service"
+        "minecraft-server-magic.socket"
+      ];
+      conflicts = [ "restic-backups-backblaze.service" ];
+      requires = [ "minecraft-before-backup.service" ];
+      wants = [ "minecraft-after-backup.service" ]; # we use wants because we want to run cleanup even on failure
+      after = [ "minecraft-before-backup.service" ];
+      before = [ "minecraft-after-backup.service" ];
+      serviceConfig = {
+        User = config.services.minecraft-servers.user;
+        Group = config.services.minecraft-servers.group;
+        Type = "oneshot";
+      };
+      script = ''
         BACKUP_PATH="/srv/minecraft/backup/$(date --iso-8601=minutes)"
         mkdir -p "$BACKUP_PATH"
         cp --reflink=always -r "$DATA_PATH" "$BACKUP_PATH"
         # for the eventual cleanup script
         ${pkgs.fd}/bin/fd --type directory --exact-depth 1 --changed-before 1d --absolute-path --full-path '/srv/minecraft/backup/' /srv/minecraft/backup
         echo "Backup done"
-        ${after-backup}
       '';
-    in
-    {
-      minecraft-before-backup = {
-        enable = true;
-        description = "Prepare for restic backup of Minecraft server";
-        requisite = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        wantedBy = [ "restic-backups-backblaze.service" ];
-        before = [ "restic-backups-backblaze.service" ];
-        after = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        serviceConfig = {
-          User = config.services.minecraft-servers.user;
-          Group = config.services.minecraft-servers.group;
-          Type = "oneshot";
-        };
-        script = builtins.readFile before-backup;
-        environment = {
-          ANNOUNCE = "1";
-          BACKUP_DEST = "Remote";
-          DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
-          SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
-          SAVE_WAIT_TIME = "60";
-          SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
-        };
-      };
-
-      minecraft-after-backup = {
-        enable = true;
-        description = "Cleanup after restic backup of Minecraft server";
-        requisite = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        wantedBy = [ "restic-backups-backblaze.service" ];
-        after = [ "restic-backups-backblaze.service" ];
-        serviceConfig = {
-          User = config.services.minecraft-servers.user;
-          Group = config.services.minecraft-servers.group;
-          Type = "oneshot";
-        };
-        script = builtins.readFile after-backup;
-        environment = {
-          ANNOUNCE = "1";
-          BACKUP_DEST = "Remote";
-          DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
-          SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
-          SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
-        };
-      };
-
-      minecraft-local-backup = {
-        enable = true;
-        description = "Backup Minecraft server to a local folder";
-        requisite = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        conflicts = [
-          "restic-backups-backblaze.service"
-          "minecraft-before-backup.service"
-          "minecraft-after-backup.service"
-        ];
-        serviceConfig = {
-          User = config.services.minecraft-servers.user;
-          Group = config.services.minecraft-servers.group;
-          Type = "oneshot";
-        };
-        script = local-backup;
-        environment = {
-          ANNOUNCE = "0";
-          BACKUP_DEST = "Local";
-          DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
-          SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
-          SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
-          SAVE_WAIT_TIME = "60";
-        };
+      environment = {
+        ANNOUNCE = "0";
+        BACKUP_DEST = "Local";
+        DATA_PATH = config.systemd.services.minecraft-server-magic.serviceConfig.WorkingDirectory;
+        SOCKET_PATH = "/run/minecraft/magic.stdin"; # trying to reference the config path failed for some reason
+        SQLITE_PATH = "${pkgs.sqlite}/bin/sqlite3";
+        SAVE_WAIT_TIME = "60";
       };
     };
+  };
 
   systemd.timers.minecraft-local-backup = {
     enable = true;
