91a6056a8dade7391dc9ab7af299e36d1a4a752f
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-06-06 17:29:20 +1200

    remove all ops

--- units/minecraft/minecraft.nix
+++ units/minecraft/minecraft.nix
@@ -33,33 +33,7 @@ in
         jre_headless = graal;
       };
       autoStart = true;
-      operators = {
-        ArcOnyx = {
-          uuid = "36322fea-3925-4ee1-a160-1f068e7cef44";
-          level = 4;
-          bypassesPlayerLimit = true;
-        };
-        Qyila = {
-          uuid = "9aba3284-1e93-4e49-a8f4-73a9fbe2a3e3";
-          level = 4;
-        };
-        Sithisilith = {
-          uuid = "b4807d95-aaf5-4654-b226-a2d278a71553";
-          level = 2;
-        };
-        FISHYNZ_ = {
-          uuid = "53a2148c-0690-4aa0-b75a-2072d78a772a";
-          level = 2;
-        };
-        SurrealThings = {
-          uuid = "574f914f-f5e1-4a3d-9942-ca11b339713d";
-          level = 3;
-        };
-        Ilphaonar = {
-          uuid = "45507f74-4d00-48cc-a9cd-3c80cbd73209";
-          level = 4;
-        };
-      };
+      operators = { };
       jvmOpts = "-Xms8G -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:AllocatePrefetchStyle=3 -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:+EagerJVMCI -Dgraal.TuneInlinerExploration=1 -Djdk.graal.CompilerConfiguration=enterprise -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:+UseTransparentHugePages -XX:ConcGCThreads=6 --enable-native-access=ALL-UNNAMED";
       serverProperties = {
         level-seed = "3353466134392672190";
