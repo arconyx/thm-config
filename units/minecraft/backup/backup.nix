8908bf87d734475f9d17aa392be5101befae738b
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-06-17 13:34:08 +1200

    dynamically create remote services for each mc server

--- units/minecraft/backup/backup.nix
+++ units/minecraft/backup/backup.nix
@@ -9,7 +9,6 @@
       "DistantHorizons.sqlite-shm"
       "DistantHorizons.sqlite-wal"
       "/srv/minecraft/backup" # don't want to waste storage backing up the local backups
-      "/srv/minecraft/magic/bluemap/" # this is basically a cache
     ];
     paths = [
       "/etc/minecraft"
@@ -17,86 +16,99 @@
     ];
   };
 
-  # TODO: Dynamically detect active servers
   systemd.services =
     let
-      server-name = "magic";
+      forEachServer =
+        f: lib.concatMapAttrs (name: value: f name value) config.services.minecraft-servers.servers;
     in
-    lib.mkIf config.services.minecraft-servers.servers.${server-name}.enable {
-      restic-backups-backblaze = {
-        conflicts = [ "minecraft-server-${server-name}.service" ];
-        before = [ "minecraft-server-${server-name}.service" ];
-        onSuccess = [ "minecraft-server-${server-name}.service" ];
-        onFailure = [ "minecraft-remote-backup-failure-warning.service" ];
-      };
+    forEachServer (
+      name: cfg:
+      let
+        socket = config.services.minecraft-servers.managementSystem.systemd-socket.stdinSocket.path name;
+      in
+      lib.mkIf cfg.enable {
+        restic-backups-backblaze = {
+          # Shutdown server during backup
+          conflicts = [ "minecraft-server-${name}.service" ];
+          # If we somehow start at the same time, run the backup first
+          before = [ "minecraft-server-${name}.service" ];
+          # Call warning script before we run
+          wants = [ "minecraft-shutdown-warning-${name}.service" ];
+          after = [ "minecraft-shutdown-warning-${name}.service" ];
+          # Restart server after, printing warning on failure
+          onSuccess = [ "minecraft-server-${name}.service" ];
+          onFailure = [
+            "minecraft-server-${name}.service"
+            "minecraft-remote-backup-failure-warning-${name}.service"
+          ];
+        };
 
-      minecraft-shutdown-warning = {
-        enable = true;
-        description = "Warn about Minecraft server '${server-name}' shutdown";
-        requisite = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        wantedBy = [ "restic-backups-backblaze.service" ];
-        before = [ "restic-backups-backblaze.service" ];
-        serviceConfig = {
-          User = config.services.minecraft-servers.user;
-          Group = config.services.minecraft-servers.group;
-          Type = "oneshot";
-        };
-        script = ''
-          if [[ -p "$SOCKET_PATH" ]]; then
-            echo "Server shutdown warning triggered"
-            if echo "say Server will shutdown for backup in $WARNING_TIME seconds." > "$SOCKET_PATH"; then
-                echo "Shutdown warning command sent successfully."
+        "minecraft-shutdown-warning-${name}" = {
+          enable = true;
+          description = "Warn about Minecraft server '${name}' shutdown";
+          requisite = [
+            "minecraft-server-${name}.service"
+            "minecraft-server-${name}.socket"
+          ];
+          serviceConfig = {
+            User = config.services.minecraft-servers.user;
+            Group = config.services.minecraft-servers.group;
+            Type = "oneshot";
+          };
+          script = ''
+            if [[ -p "$SOCKET_PATH" ]]; then
+              echo "Server shutdown warning triggered"
+              if echo "say Server will shutdown for backup in $WARNING_TIME seconds." > "$SOCKET_PATH"; then
+                  echo "Shutdown warning command sent successfully."
+              else
+                  echo "Warning: Failed to send shutdown warning via socket."
+              fi
+              sleep "$WARNING_TIME"
             else
-                echo "Warning: Failed to send shutdown warning via socket."
+                echo "Warning: Socket file '$SOCKET_PATH' not found or is not a socket."
+                echo "Minecraft server appears to be offline or socket is not active."
+                echo "Backup will proceed regardless."
             fi
-            sleep "$WARNING_TIME"
-          else
-              echo "Warning: Socket file '$SOCKET_PATH' not found or is not a socket."
-              echo "Minecraft server appears to be offline or socket is not active."
-              echo "Backup will proceed regardless."
-          fi
-        '';
-        environment = {
-          SOCKET_PATH = "/run/minecraft/${server-name}.stdin"; # trying to reference the config path failed for some reason
-          WARNING_TIME = "600";
+          '';
+          environment = {
+            SOCKET_PATH = "${socket}";
+            WARNING_TIME = "600";
+          };
         };
-      };
 
-      minecraft-remote-backup-failure-warning = {
-        enable = true;
-        description = "Warn about failed remote backup of Minecraft server '${server-name}'";
-        wants = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        after = [
-          "minecraft-server-magic.service"
-          "minecraft-server-magic.socket"
-        ];
-        serviceConfig = {
-          User = config.services.minecraft-servers.user;
-          Group = config.services.minecraft-servers.group;
-          Type = "oneshot";
-        };
-        script = ''
-          if [[ -p "$SOCKET_PATH" ]]; then
-            echo "Attempting to send ingame notification of failed backup."
-            if echo "WARNING: Server backup failed!" > "$SOCKET_PATH"; then
-                echo "Warning sent successfully."
+        "minecraft-remote-backup-failure-warning-${name}" = {
+          enable = true;
+          description = "Warn about failed remote backup of Minecraft server '${name}'";
+          requires = [
+            "minecraft-server-${name}.service"
+            "minecraft-server-${name}.socket"
+          ];
+          after = [
+            "minecraft-server-${name}.service"
+            "minecraft-server-${name}.socket"
+          ];
+          serviceConfig = {
+            User = config.services.minecraft-servers.user;
+            Group = config.services.minecraft-servers.group;
+            Type = "oneshot";
+          };
+          script = ''
+            if [[ -p "$SOCKET_PATH" ]]; then
+              echo "Attempting to send in-game notification of failed backup."
+              if echo "WARNING: Server backup failed!" > "$SOCKET_PATH"; then
+                  echo "Warning sent successfully."
+              else
+                  echo "Warning: Failed to send backup failure warning via socket."
+              fi
             else
-                echo "Warning: Failed to send backup failure warning via socket."
+                echo "Warning: Socket file '$SOCKET_PATH' not found or is not a socket."
+                echo "Minecraft server appears to be offline or socket is not active."
             fi
-          else
-              echo "Warning: Socket file '$SOCKET_PATH' not found or is not a socket."
-              echo "Minecraft server appears to be offline or socket is not active."
-          fi
-        '';
-        environment = {
-          SOCKET_PATH = "/run/minecraft/${server-name}.stdin"; # trying to reference the config path failed for some reason
+          '';
+          environment = {
+            SOCKET_PATH = "${socket}";
+          };
         };
-      };
-    };
+      }
+    );
 }
