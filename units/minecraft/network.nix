255e74e9ec04ad1fbfc93ca3b3cd8e93589ac5ca
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-08-29 18:37:47 +1200

    Disable caddy properly

--- units/minecraft/network.nix
+++ units/minecraft/network.nix
@@ -13,32 +13,32 @@
   };
 
   # TODO: Reenable after exposing via cloudflare tunnel
-  services.caddy.enable = false;
-  services.caddy.virtualHosts.":9010" = {
-    extraConfig = ''
-      encode
+  # services.caddy.enable = true;
+  # services.caddy.virtualHosts.":9010" = {
+  #   extraConfig = ''
+  #     encode
 
-      redir /exposure /exposure/
-      redir /map /map/
+  #     redir /exposure /exposure/
+  #     redir /map /map/
 
-      # let people browse their image exports
-      handle_path /exposure/* {
-        root * /srv/minecraft/magic/world/exposures
-        file_server browse
-      }
+  #     # let people browse their image exports
+  #     handle_path /exposure/* {
+  #       root * /srv/minecraft/magic/world/exposures
+  #       file_server browse
+  #     }
 
-      handle {
-        error 404
-      }
+  #     handle {
+  #       error 404
+  #     }
 
-      handle_errors {
-        respond "{err.status_code} {err.status_text}"
-      }
-    '';
-  };
+  #     handle_errors {
+  #       respond "{err.status_code} {err.status_text}"
+  #     }
+  #   '';
+  # };
 
-  # so we don't have to chmod exposures world readable
-  systemd.services.caddy.serviceConfig = {
-    SupplementaryGroups = [ "minecraft" ];
-  };
+  # # so we don't have to chmod exposures world readable
+  # systemd.services.caddy.serviceConfig = {
+  #   SupplementaryGroups = [ "minecraft" ];
+  # };
 }
