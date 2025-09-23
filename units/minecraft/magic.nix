8314d58144a44cf93cdcb703448401d2852c0889
ArcOnyx <arconyx@users.noreply.github.com>
2025-09-24 10:46:06 +1200

    Increase mc idle shutdown time to 10min

--- units/minecraft/magic.nix
+++ units/minecraft/magic.nix
@@ -64,7 +64,7 @@ in
   systemd.timers.stop-minecraft = {
     enable = true;
     timerConfig = {
-      OnActiveSec = "5min";
+      OnActiveSec = "10min";
       OnUnitInactiveSec = "10min";
       Unit = "stop-minecraft.service";
     };
