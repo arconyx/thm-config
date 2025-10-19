24de2a65842717290f5a19d3e7c2be76aad0b804
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-10-19 17:09:42 +1300

    It turns out HM doesn't have nice systemd config :(

--- units/space-engineers/default.nix
+++ units/space-engineers/default.nix
@@ -1,9 +1,18 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 
 let
   default_world = ./inital_state;
   home = "/var/lib/space-engineers";
   world_host_dir = "${home}/se_world";
+  init-world = pkgs.writeShellScript "init-world" ''
+    set -eo pipefail
+    mkdir -p "${world_host_dir}"
+    if [ ! -d "${world_host_dir}/World" ]; then
+      cp -r --update=none "${default_world}/World" "${world_host_dir}"
+    fi
+    cp --update=all "${default_world}/SpaceEngineers-Dedicated.cfg" "${world_host_dir}"
+    chmod -R a=rX,u+w "${world_host_dir}"
+  '';
 in
 {
   users.users.engineer = {
@@ -67,19 +76,47 @@ in
         };
       };
 
+      systemd.user.services.pod-unifi = {
+
+        Service = {
+          Type = "forking";
+          ExecStartPre = [
+            # This is needed for the Pod start automatically
+            "${pkgs.coreutils}/bin/sleep 3s"
+            # Port config see:
+            # https://help.ui.com/hc/en-us/articles/218506997-Required-Ports-Reference
+            # The image requires `--userns=host`
+            ''
+              -${pkgs.podman}/bin/podman pod create --replace \
+                --network=unifi \
+                --userns=host \
+                --cpus=3 \
+                --label=PODMAN_SYSTEMD_UNIT="pod-unifi.service" \
+                -p 192.168.3.1:8443:8443/tcp \
+                -p 192.168.4.1:1900:1900/udp \
+                -p 192.168.4.1:3478:3478/udp \
+                -p 192.168.4.1:5514:5514/udp \
+                -p 192.168.4.1:10001:10001/udp \
+                -p 192.168.4.1:6789:6789/tcp \
+                -p 192.168.4.1:8080:8080/tcp unifi
+            ''
+          ]; # ExecStartPre
+          ExecStart = "${pkgs.podman}/bin/podman pod start unifi";
+          ExecStop = "${pkgs.podman}/bin/podman pod stop unifi";
+          RestartSec = "1s";
+        }; # Service
+      }; # systemd.user.services.pod-unifi
+
       systemd.user.services.init-se-world = {
-        enable = true;
-        script = ''
-          set -eo pipefail
-          mkdir -p "${world_host_dir}" $VERBOSE_ARG
-          if [ ! -d "${world_host_dir}/World" ]; then
-            cp -r --update=none $VERBOSE_ARG "${default_world}/World" "${world_host_dir}"
-          fi
-          cp --update=all $VERBOSE_ARG "${default_world}/SpaceEngineers-Dedicated.cfg" "${world_host_dir}"
-          chmod -R a=rX,u+w $VERBOSE_ARG "${world_host_dir}"
-        '';
-        requiredBy = [ "podman-${container-name}.service" ];
-        before = [ "podman-${container-name}.service" ];
+        Unit = {
+          Description = "Initialise Space Engineers world";
+          Before = [ "podman-${container-name}.service" ];
+        };
+        Install.RequiredBy = [ "podman-${container-name}.service" ];
+        Service = {
+          Type = "exec";
+          ExecStart = init-world;
+        };
       };
 
     };
