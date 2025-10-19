2614e4df6ac0a1d0ee79192bfab5f188ccc2bbfd
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-10-19 16:53:27 +1300

    Replace activation script with systemd service

--- units/space-engineers/default.nix
+++ units/space-engineers/default.nix
@@ -1,23 +1,9 @@
-# Auto-generated using compose2nix v0.3.3-pre.
-# TODO: Switch to rootless podman
-{
-  lib,
-  ...
-}:
+{ lib, ... }:
 
 let
   default_world = ./inital_state;
   home = "/var/lib/space-engineers";
   world_host_dir = "${home}/se_world";
-  init-world = pkgs.writeShellScript "init-world" ''
-    set -eo pipefail
-    mkdir -p "${world_host_dir}" $VERBOSE_ARG
-    if [ ! -d "${world_host_dir}/World" ]; then
-      cp -r --update=none $VERBOSE_ARG "${default_world}/World" "${world_host_dir}"
-    fi
-    cp --update=all $VERBOSE_ARG "${default_world}/SpaceEngineers-Dedicated.cfg" "${world_host_dir}"
-    chmod -R a=rX,u+w $VERBOSE_ARG "${world_host_dir}"
-  '';
 in
 {
   users.users.engineer = {
@@ -37,7 +23,9 @@ in
   systemd.enableStrictShellChecks = lib.mkForce false;
 
   home-manager.users.engineer =
-    { lib, ... }:
+    let
+      container-name = "space-engineers";
+    in
     {
       home.stateVersion = "25.05";
       home.homeDirectory = home;
@@ -62,7 +50,7 @@ in
           };
         };
 
-        containers."space-engineers" = {
+        containers."${container-name}" = {
           autoStart = false;
           description = "Space Engineers server";
           image = "mmmaxwwwell/space-engineers-dedicated-docker-linux:v2";
@@ -75,14 +63,25 @@ in
             "spaceengineers_steamcmd:/home/wine:rw"
             "${world_host_dir}:/appdata/space-engineers/World:rw"
           ];
+          userNS = "keep-id:uid=1000,gid=1000";
         };
       };
 
-      home.activation = {
-        initSEWorld = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          run ${init-world}
+      systemd.services.init-se-world = {
+        enable = true;
+        script = ''
+          set -eo pipefail
+          mkdir -p "${world_host_dir}" $VERBOSE_ARG
+          if [ ! -d "${world_host_dir}/World" ]; then
+            cp -r --update=none $VERBOSE_ARG "${default_world}/World" "${world_host_dir}"
+          fi
+          cp --update=all $VERBOSE_ARG "${default_world}/SpaceEngineers-Dedicated.cfg" "${world_host_dir}"
+          chmod -R a=rX,u+w $VERBOSE_ARG "${world_host_dir}"
         '';
+        requiredBy = [ "podman-${container-name}.service" ];
+        before = [ "podman-${container-name}.service" ];
       };
+
     };
 
   networking.firewall.allowedUDPPorts = [ 25565 ];
