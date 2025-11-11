38f8fab734e4859aaaf26fdddda3d6e55db03c30
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-11-11 20:40:44 +1300

    Move saves into expected folder

--- units/space-engineers/default.nix
+++ units/space-engineers/default.nix
@@ -5,13 +5,13 @@ let
   home = "/var/lib/space-engineers";
 
   data_dir = "${home}/se_instances";
-  instance_name = "REV_THM_Server";
+  instance_name = "server";
   instance_dir = "${data_dir}/${instance_name}";
   init-world = pkgs.writeShellScript "init-world" ''
     set -eo pipefail
     if [ ! -d "${instance_dir}" ]; then
-      ${pkgs.coreutils}/bin/mkdir -p "${instance_dir}"
-      ${pkgs.coreutils}/bin/cp -r --update=none "${default_world}/World" "${instance_dir}"
+      ${pkgs.coreutils}/bin/mkdir -p "${instance_dir}/Saves"
+      ${pkgs.coreutils}/bin/cp -r --update=none "${default_world}/World" "${instance_dir}/Saves/StarSystem"
       ${pkgs.coreutils}/bin/cp --update=all "${default_world}/SpaceEngineers-Dedicated.cfg" "${instance_dir}"
       ${pkgs.coreutils}/bin/chmod -R a=rX,u+w "${data_dir}"
     fi
