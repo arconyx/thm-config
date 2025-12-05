08d43590f7e2021d22249b8c2785ed7cf46a5329
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-12-05 14:07:39 +1300

    Move magic stuff into magic.nix

--- hosts/hive/minecraft/default.nix
+++ hosts/hive/minecraft/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ ... }:
 {
   imports = [ ./servers/magic.nix ];
 
@@ -27,50 +27,11 @@
     frequency = "*:0/15";
     domains = [
       "mc.thehivemind.gay"
+      "*.mc.thehivemind.gay"
     ];
     proxied = false; # no point trying to proxy minecraft
     deleteMissing = false;
     apiTokenFile = "/etc/cloudflare/apikey.env";
-  };
-
-  # make exposures photos accessible on the interwebs
-  services.caddy.enable = true;
-  services.caddy.virtualHosts."hive.thehivemind.gay:80" = {
-    extraConfig = ''
-      encode
-
-      handle /hooks/* {
-        reverse_proxy :${builtins.toString config.services.webhook.port}
-      }
-
-      # let people browse their image exports
-      redir /exposure /exposure/
-      handle_path /exposure/* {
-        root * /srv/minecraft/magic/world/exposures
-        file_server browse
-      }
-
-      # this handle matches all requests so anything else
-      # needs to be in a handle or handle_path block of their own
-      handle {
-        error 404
-      }
-
-      handle_errors {
-        respond "{err.status_code} {err.status_text}"
-      }
-    '';
-  };
-  # so we don't have to chmod exposures world readable
-  systemd.services.caddy.serviceConfig = {
-    SupplementaryGroups = [ "minecraft" ];
-  };
-  services.cloudflared = {
-    enable = true;
-    tunnels.hive = {
-      default = "http_status:404";
-      credentialsFile = "/etc/cloudflare/tunnel_credentials.json";
-    };
   };
 
   thm.services.minecraft = {
