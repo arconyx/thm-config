9aff075cf01397f4b70a5fd9ec219ff20f85236e
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-03-12 14:51:59 +1300

    hive: Enable accept routes in tailscale

--- hosts/hive/default.nix
+++ hosts/hive/default.nix
@@ -26,7 +26,8 @@
     variant = "";
   };
 
-  services.minecraft-servers.servers.magic.enable = true;
+  # Needed to access tailscale serve routes prior to v1.94
+  services.tailscale.extraSetFlags = [ "--accept-routes" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -35,5 +36,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
