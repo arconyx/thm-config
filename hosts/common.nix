e2f8aca6baea6b9c55777bc304ecfefbe653f9b8
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-05-04 12:05:42 +1200

    Revert "Mitigate CVE-2026-31431"
    This reverts commit 580fdd499a43851c5321b16d54e982dce149f747.

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./../home
@@ -13,12 +13,4 @@
       statusEnvFile = "/etc/backblaze/sentinel.env";
     };
   };
-
-  # Mitigate CVE-2026-31431
-  # See affected version here: https://www.cve.org/CVERecord/?id=CVE-2026-31431
-  # TODO: Remove once patch has made it into used kernel versions
-  # Blacklisting disables automatic loading
-  boot.blacklistedKernelModules = [ "algif_aead" ];
-  # When manually loading run a dummy program instead
-  boot.extraModprobeConfig = "install algif_aead ${pkgs.coreutils}/bin/false\n";
 }
