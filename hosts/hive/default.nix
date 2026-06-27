53f37627c8184e2ba6f0c0e2d6c7940db41d7b72
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-06-27 18:45:55 +1200

    wiki: Enable wiki on hive

--- hosts/hive/default.nix
+++ hosts/hive/default.nix
@@ -26,6 +26,12 @@
     variant = "";
   };
 
+  thm.services.wiki = {
+    enable = true;
+    domain = "wiki.thehivemind.gay";
+    articlePath = "w";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
